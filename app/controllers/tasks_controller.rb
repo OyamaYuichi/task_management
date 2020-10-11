9d9bbaea890186bbb9a86e1c771c5c29ea4b15ad
yuichi oyama <yuichi19991208@gmail.com>
2020-10-12 00:09:18 +0900

    新規画面と編集画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,43 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :show, :destroy]
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
   
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:name, :detail)
+  end
 end
