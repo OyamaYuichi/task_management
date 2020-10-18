b0e1d0c9b161f5f0787104d753ec0a957b146c9d
yuichi oyama <yuichi19991208@gmail.com>
2020-10-18 19:03:31 +0900

    ユーザー一覧画面の作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,58 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all.includes(:tasks)
+  end
+
+  def new
+    if logged_in?
+      flash[:notice] = '既にアカウントをお持ちです'
+      redirect_to tasks_path
+    end
+      @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'ユーザー情報を変更しました'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @user.destroy
+    redirect_to new_user_path, notice: "アカウントを削除しました"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to tasks_path
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users, only: [:new, :create, :show, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
+
+  namespace :admin do
+    resources :users, except: [:show]
+  end
 end
