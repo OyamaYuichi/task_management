7e54b881c85eb0a92cc729d3e2681cd16eb07601
yuichi oyama <yuichi19991208@gmail.com>
2020-10-18 23:58:30 +0900

    最初の管理者をseedデータで作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :require_admin
 
   def index
     @users = User.all.includes(:tasks)
@@ -13,7 +14,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
@@ -48,7 +49,11 @@ class Admin::UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
   end
 
 end
